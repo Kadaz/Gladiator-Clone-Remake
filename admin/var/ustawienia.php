bfe9c67bf975d645df3139d1c9990086e1574c35
TolisM <Kadaz@users.noreply.github.com>
2025-05-19 01:06:07 +0300

    Add files via upload

---
+++ admin/var/ustawienia.php
@@ -0,0 +1,12 @@
+﻿<?php
+$host_bazy_danych = 'localhost'; // Host predeterminado del MYSQL
+$uzytkownik_bazy_danych = 'root'; // Usuario del MYSQl
+$haslo_bazy_danych = '3227'; // Contraseña para el usuario del MYSQL
+$nazwa_bazy_danych = 'gladiatus';  // Nombre de la DB
+
+$polacz = mysql_connect($host_bazy_danych, $uzytkownik_bazy_danych, $haslo_bazy_danych) or die('Error');
+
+mysql_select_db($nazwa_bazy_danych, $polacz) or die('Error');
+
+mysql_query("SET NAMES 'utf8'");
+?>
